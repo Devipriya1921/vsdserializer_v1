c4f454ce9e3971aa879a1c43afb895d55e73d246
A Devipriya <83152452+Devipriya1921@users.noreply.github.com>
2021-08-27 17:27:13 +0530

    post_layout

---
+++ post_layout/gls.v
@@ -0,0 +1,28 @@
+`include "vsdserializer_v1.synthesis.v"
+`timescale 1ns / 1ps
+module vsdserializer_v1_tb();
+reg clk,load;
+reg[9:0] INPUT;
+wire OUTPUT;
+vsdserializer_v1 dut (clk, load, INPUT, OUTPUT);
+initial begin
+ clk = 1;
+ INPUT = $urandom;
+ load = 1;
+ #30 load = 0;
+ #120 INPUT = $urandom;
+load = 1;
+#30 load = 0;
+#120 INPUT = $urandom;
+load = 1;
+#30 load = 0;
+end
+always #50 clk = ~clk;
+initial begin
+$dumpfile("vsdserializer_v1.vcd");
+$dumpvars(0,vsdserializer_v1_tb);
+#900 $finish;
+end
+endmodule
+
+
