5a780797a26aafd19eff311c7ef6be4c41be0113
A Devipriya <83152452+Devipriya1921@users.noreply.github.com>
2021-11-01 17:13:34 +0530

    RTL

---
+++ rtl/user_proj_example.v
@@ -0,0 +1,56 @@
+// SPDX-FileCopyrightText: 2020 Efabless Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// SPDX-License-Identifier: Apache-2.0
+
+
+/*
+ *-------------------------------------------------------------
+ *
+ * user_proj_example
+ *
+ * This is an example of a (trivially simple) user project,
+ * showing how the user project can connect to the logic
+ * analyzer, the wishbone bus, and the I/O pads.
+ *
+ * This project generates an integer count, which is output
+ * on the user area GPIO pads (digital output only).  The
+ * wishbone connection allows the project to be controlled
+ * (start and stop) from the management SoC program.
+ *
+ * See the testbenches in directory "mprj_counter" for the
+ * example programs that drive this user project.  The three
+ * testbenches are "io_ports", "la_test1", and "la_test2".
+ *
+ *-------------------------------------------------------------
+ */
+
+`default_nettype none
+
+module user_proj_example (clk, load, INPUT, OUTPUT);
+input clk, load;
+input [9:0] INPUT;
+output reg OUTPUT;
+reg [9:0] tmp;
+always @(posedge clk)
+begin
+  if(load)
+  tmp<=INPUT;
+  else
+  begin
+  OUTPUT <= tmp[9];
+  tmp <= {tmp[8:0],1'b0};
+  end
+end
+endmodule
+`default_nettype wire
