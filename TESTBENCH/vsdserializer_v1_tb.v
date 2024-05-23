83f0b4bc10e8299300290a4b956a570f5a4a3e19
A Devipriya <83152452+Devipriya1921@users.noreply.github.com>
2024-05-23 14:37:52 +0530

    Update vsdserializer_v1_tb.v

--- TESTBENCH/vsdserializer_v1_tb.v
+++ TESTBENCH/vsdserializer_v1_tb.v
@@ -1,26 +1,43 @@
 `timescale 1ns / 1ps
 `include "vsdserializer_v1.v"
-module vsdserializer_v1_tb();
-reg clk,load;
-reg[9:0] INPUT;
-wire OUTPUT;
-vsdserializer_v1 dut (clk, load, INPUT, OUTPUT);
-initial begin
- clk = 1;
- INPUT = $urandom;
- load = 1;
- #30 load = 0;
- #120 INPUT = $urandom;
-load = 1;
-#30 load = 0;
-#120 INPUT = $urandom;
-load = 1;
-#30 load = 0;
+module vsdserializer_v1_tb;
+ 
+reg clk = 1'b1;
+reg load = 1'b0;
+reg rst_n = 1'b1;
+reg[9:0] data_in = 10'd0;
+wire data_out;
+ 
+vsdserializer_v1 dut (clk, rst_n, load, data_in, data_out);
+ 
+always begin
+#2 clk = ~clk;
 end
-always #50 clk = ~clk;
+ 
+initial begin
+#10 rst_n = 1'b0;
+#10 rst_n = 1'b1;
+ 
+#20;
+ 
+@(posedge clk) begin
+data_in = $urandom;
+load = 1'b1;
+end
+ 
+#1;
+ 
+@(posedge clk) begin
+data_in = 10'd0;
+load = 1'b0;
+end
+ 
+#100 $finish;
+end
+ 
 initial begin
 $dumpfile("vsdserializer_v1_tb.vcd");
 $dumpvars(0,vsdserializer_v1_tb);
-#900 $finish;
 end
+ 
 endmodule
